8e887a447c941db7866b47b926b10b732325c952
Klove Adams <kloveadams@gmail.com>
2021-09-21 18:50:58 -0400

    Login without conflicts for real

--- app-name/src/components/Login.js
+++ app-name/src/components/Login.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 import schema from './formSchema';
 import axios from 'axios';
 // import App  from '../App';
-import '../Login.css';
+// import '../Login.css';
 
 const initialFormValues = {
   username: '',  
