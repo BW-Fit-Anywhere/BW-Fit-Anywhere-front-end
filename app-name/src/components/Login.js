5bd42a708d9408297491715e86bbf96c39f94de2
Klove Adams <kloveadams@gmail.com>
2021-09-21 18:45:06 -0400

    Login without conflicts

--- app-name/src/components/Login.js
+++ app-name/src/components/Login.js
@@ -1,15 +1,98 @@
-import React, { useState } from "react";
-// import axios from 'axios';
-// import axiosWithAuth from '../utils/axiosWithAuth';
-// import { useHistory } from 'react-router';
+import React, { useState, useEffect } from "react";
+import * as yup from 'yup';
+import schema from './formSchema';
+import axios from 'axios';
+// import App  from '../App';
+import '../Login.css';
 
 const initialFormValues = {
-  username: "",
-  password: ""
+  username: '',  
+  password: ''
 }
 
+const initialFormErrors = {
+  username: '',
+  password: ''
+}
+
+// const initialClients = []
+const initialDisabled = true
+
 const Login = () => {
+
+  const [clients, setClients] = useState([])  
   const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled) 
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues,
+      [name]: value 
+    })
+  }
+
+  const postClient = newClient => {
+    axios.post('', newClient)
+      .then(res => {
+        setClients([res.data, ...clients]);
+        setFormValues(initialFormValues);
+      }).catch(err => {
+        console.error(err);
+        setFormValues(initialFormValues);
+      })
+  }
+
+  const formSubmit = (e) => {
+   e.preventDefault();
+   const newClient = {
+      username: formValues.username.trim(),
+      password: formValues.password.trim()
+    }
+   return setClients(newClient)
+  }
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
+
+  return (
+    <div className='container'>
+      <header><h1>Login</h1></header>
+      <form onSubmit={formSubmit}>
+        <label>
+          <input
+            id="username"
+            value={formValues.username}
+            name="username"
+            type="text"
+            onChange={inputChange}
+          />
+         <br></br>
+        </label>
+        <label>
+          <input
+            id="password"
+            value={formValues.password}
+            password="password"
+            type="password"
+            onChange={inputChange}
+          />
+        </label>
+        <br></br>
+        <button>Login</button>
+      </form>
+    
+    </div>
+  )
 
 }
 export default Login;
