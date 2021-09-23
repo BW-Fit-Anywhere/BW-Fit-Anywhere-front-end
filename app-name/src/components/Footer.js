9300dd43b01511d36d626d646763b614dce60dce
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-09-22 20:48:13 -0400

    6

--- app-name/src/components/Footer.js
+++ app-name/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
      <footer className="footer">
         <h3>Anywhere Fitness APP Copyright 2021</h3> 
-        <p>Build Team: Jeong Pak - Klove Adams - Leo Oládimú <br/> Michael Guerrero - Priscila Monteiro - Rimsha Saleem.</p>
+        <p>Build Team: Jeong Pak - Klove Adams - Leo Ọládimú <br/> Michael Guerrero - Priscila Monteiro - Rimsha Saleem.</p>
       
       </footer>
   )
--- app-name/src/components/formSchema.js
+++ app-name/src/components/formSchema.js
@@ -3,12 +3,14 @@ import * as yup from 'yup';
 const formSchema = yup.object().shape({
     username: yup
         .string()
+        .min(5, '5 Letters Or Die!!!')
         .trim()
         .required('☠︎Type Your Name!☠︎'),
-    // password: yup
-    //     .string()
-    //     .password('Valid Password Or Die!!!')
-    //     .required('☠︎☠︎☠︎')
+    password: yup
+        .string()
+        .min(5, '5 Letters Or Die!!!')
+        .trim()
+        .required('☠︎Valid Password Or Die!!!☠︎')
     
 })
 
